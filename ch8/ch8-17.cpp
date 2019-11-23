c7ffa7ce6ced307cf4f52a0d2cb0b9417b92b0db
25077667 <zxc25077667@gmail.com>
2019-11-24 00:57:04 +0800

    add p17

---
+++ ch8/ch8-17.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include "statistics.hpp"
+using namespace std;
+// B073040047 楊志璿
+
+int main() {
+    vector<double> dataSet;
+    string title = readSingleLineCSV(dataSet, "ch8-17.csv");
+
+    double alpha;
+    cout << "what's alpha: ";
+    cin >> alpha;
+    double mean = genMean(dataSet);
+    auto confidence = genConfidenceInterval(mean, errorRadius(dataSet, alpha / 2));
+
+    cout << "In " << title << endl
+         << "Sample Mean:" << mean << endl
+         << confidence << endl;
+    return 0;
+}
