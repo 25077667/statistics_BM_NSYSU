4efece80ac3f2798c5b26eee35d04e5c055fe08d
25077667 <zxc25077667@gmail.com>
2019-11-23 13:46:12 +0800

    ch8-9 using hpp

--- ch8/ch8-9.cpp
+++ ch8/ch8-9.cpp
@@ -4,24 +4,17 @@ using namespace std;
 
 int main() {
     vector<double> dataSet;
-    double newData;
-
-    ifstream inFile("TobaccoFires.csv", ios::in);
-    while (inFile >> newData)
-        dataSet.push_back(newData);
+    string title = readSingleLineCSV(dataSet, "TobaccoFires.csv");
 
     double alpha;
     cout << "what's alpha: ";
     cin >> alpha;
-    double mean = genMean(dataSet),
-           sampleSize = dataSet.size(),
-           ssd = genSampleStandardDeviation(dataSet, mean, sampleSize),
-           z = genZValue(alpha);
+    double mean = genMean(dataSet);
+    auto confidence = genConfidenceInterval(mean, errorRadius(mean, alpha, dataSet.size()));
 
+    cout << "In " << title << endl;
     cout << "Sample Mean:" << mean << endl;
-    cout << "In " << (1 - alpha) * 100 << "% confidence interval mean will in: "
-         << "[ " << mean - (ssd * sqrt(sampleSize) / z) << ", " << mean + (ssd * sqrt(sampleSize) / z) << " ]" << endl;
+    cout << "In " << (1 - alpha) * 100 << "% confidence interval mean will in: " << confidence << endl;
 
-    inFile.close();
     return 0;
 }
