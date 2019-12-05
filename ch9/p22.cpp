d910d8e9f70030127f42c6fff5a2ea58cc2eac41
25077667 <zxc25077667@gmail.com>
2019-12-05 16:35:54 +0800

    need to modify hpp

--- ch9/p22.cpp
+++ ch9/p22.cpp
@@ -4,13 +4,13 @@ using namespace std;
 int main() {
     double psd = 3.2, mu_0 = 8, alpha = 0.05, x_bar = 8.4;
     int sampleSize = 120;
-    cout << "this is problem 20" << endl;
+    cout << "this is problem 22" << endl;
 
     auto test_statistic = testStatistic(x_bar, mu_0, psd, sampleSize);
     auto p_value = genPValue(test_statistic);
     cout << "In (a) please look hand-write paper." << endl;
     cout << "In (b) test statistic is " << test_statistic << " and p-value is " << p_value << endl;
-    cout << "In (c) we do " << ((p_value < alpha / 2) ? "" : "not ") << "reject H0" << endl;
+    cout << "In (c) we do " << ((p_value < alpha) ? "" : "not ") << "reject H0" << endl;
     cout << "In (d) " << (1 - alpha) * 100 << "% confidence interval for the popupation mean is in "
          << genConfidenceInterval(x_bar, errorRadius(psd, alpha, sampleSize))  << " minutes " << endl;
     return 0;
