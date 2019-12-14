bdbe58b5d0f0bf53b14d51b59b9ad09b51a65db9
25077667 <zxc25077667@gmail.com>
2019-12-14 14:10:26 +0800

    update p-value in useage

--- ch9/p17.cpp
+++ ch9/p17.cpp
@@ -7,11 +7,11 @@ int main() {
     cout << "this is problem 17" << endl;
 
     auto test_statistic = testStatistic(x_bar, mu_0, psd, sampleSize);
-    auto p_value = genPValue(test_statistic);
+    auto p_value = genPValue(test_statistic, true);
     cout << "In (a) please look hand-write paper." << endl;
     cout << "In (b) test statistic is " << test_statistic << " and p-value is " << p_value << endl;
 
-    cout << "In (c) we do " << (!(p_value < alpha / 2) ? "" : "not ") << "reject H0" << endl;
+    cout << "In (c) we do " << (!(p_value < alpha) ? "" : "not ") << "reject H0" << endl;
     cout << "In (d) " << genConfidenceInterval(x_bar, errorRadius(psd, alpha, sampleSize)) << endl;
     return 0;
 }
--- ch9/p22.cpp
+++ ch9/p22.cpp
@@ -7,11 +7,11 @@ int main() {
     cout << "this is problem 22" << endl;
 
     auto test_statistic = testStatistic(x_bar, mu_0, psd, sampleSize);
-    auto p_value = genPValue(test_statistic);
+    auto p_value = genPValue(test_statistic, true);
     cout << "In (a) please look hand-write paper." << endl;
     cout << "In (b) test statistic is " << test_statistic << " and p-value is " << p_value << endl;
     cout << "In (c) we do " << ((p_value < alpha) ? "" : "not ") << "reject H0" << endl;
     cout << "In (d) " << (1 - alpha) * 100 << "% confidence interval for the popupation mean is in "
-         << genConfidenceInterval(x_bar, errorRadius(psd, alpha, sampleSize))  << " minutes " << endl;
+         << genConfidenceInterval(x_bar, errorRadius(psd, alpha, sampleSize)) << " minutes " << endl;
     return 0;
 }
