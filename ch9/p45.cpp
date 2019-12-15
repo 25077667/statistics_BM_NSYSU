1c5c917d8169d04dd8c0ed32411d2b31657899ed
25077667 <zxc25077667@gmail.com>
2019-12-15 22:54:36 +0800

    update all

---
+++ ch9/p45.cpp
@@ -0,0 +1,25 @@
+#include "../statistics.hpp"
+using namespace std;
+
+int main() {
+    int sampleSize = 300;
+    double p_0 = 0.39, alpha = 0.05, p_bar = 0.35;
+    double sd = genPercentageStandardDeviation(p_0, sampleSize);
+    cout << "this is problem 44" << endl;
+
+    auto test_statistic = (p_bar - p_0) / sd;
+    auto p_value = genPValue(test_statistic);
+    cout << "In (a) test statistic is " << test_statistic << " and p-value is " << p_value << endl;
+    cout << "\t we do " << ((p_value < alpha) ? "" : "not ") << "reject H0" << endl;
+
+    p_0 = 0.3;
+    alpha = 0.01;
+    p_bar = 0.399;
+    sd = genPercentageStandardDeviation(p_0, sampleSize);
+    test_statistic = (p_bar - p_0) / sd;
+    p_value = genPValue(test_statistic);
+    cout << "In (a) test statistic is " << test_statistic << " and p-value is " << p_value << endl;
+    cout << "\t we do " << ((p_value < alpha) ? "" : "not ") << "reject H0" << endl;
+
+    return 0;
+}
